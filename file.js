7adefef75f4c8e362cd68692f45cd6001d4d00b2
Ranjeet Barsa <ranjeet.barsa@brainvire.com>
2024-06-01 19:27:26 +0530

    push files

---
+++ file.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const yd = require("ytdl-core");
+
+const downloadFolder = "/home/ranjeet_barsa/Music/sad";
+
+const download = async (URL) => {
+  // audio
+  // const info = await yd.getBasicInfo(URL, { quality: "highestaudio", filter: "audioonly" });
+
+  // video
+  const info = await yd.getBasicInfo(URL, { quality: "highestaudio" });
+
+  const title = info.player_response.videoDetails.title.replace(/  /g, " ").replace(/ /g, "-");
+  yd(URL, { quality: "highestaudio", filter: "audioonly" })
+    .pipe(fs.createWriteStream(`${downloadFolder}/${title}.mp3`));
+};
+
+const urls = fs.readFileSync("urls.txt", { encoding: "utf-8" }).split('\n');
+const downloadPromise = urls.map(url => download(url));
+Promise.all(downloadPromise);
