dbe9c1566cc493ea4b837fea360349a6c9d3225b
Your Name <barsaranjeet15@gmail.com>
2024-10-22 11:01:29 +0530

    changes

--- file.js
+++ file.js
@@ -6,17 +6,17 @@ const download = async (URL) => {
 
   const type = process.argv[3];
   const downloadFolder = `./${type ? type: 'videos'}`;
-  let info;
+  let contentFormat;
   if (type === 'audios')
     // audio
-    info = await yd.getBasicInfo(URL, { quality: "highestaudio", filter: "audioonly" });
+  contentFormat = { quality: "highestaudio", filter: "audioonly" };
   else
     // video
-    info = await yd.getBasicInfo(URL, { quality: "highestaudio" });
-
-  const title = info.player_response.videoDetails.title.replace(/  /g, " ").replace(/ /g, "-");
-  yd(URL, { quality: "highestaudio", filter: "audioonly" })
-    .pipe(fs.createWriteStream(`${downloadFolder}/${title}.mp3`));
+    contentFormat = { quality: "highestaudio" };
+  const info = await yd.getBasicInfo(URL, contentFormat);
+  const title = `${info.player_response.videoDetails.title.replace(/  /g, " ").replace(/ /g, "-")}/${(type === 'audios')? '.mp3' : '.mp4'}`;
+  yd(URL, contentFormat)
+    .pipe(fs.createWriteStream(`${downloadFolder}/${title}`));
 };
 const urls = fs.readFileSync("urls.txt", { encoding: "utf-8" }).split('\n');
 const downloadPromise = urls.map(url => url && download(url));
